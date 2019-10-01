d619074eb6f32966dca9920281721ebb328e3f43
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2019-09-30 22:37:32 -0700

    Updates

---
+++ precache-manifest.e42c6a228ca60bf98886eb164e087485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac5e480731f38b42781",
+    "url": "/AudioAnnotator/static/js/runtime~main.6a88d765.js"
+  },
+  {
+    "revision": "30ae5137e6e6aa2e66db",
+    "url": "/AudioAnnotator/static/js/main.24f8d392.chunk.js"
+  },
+  {
+    "revision": "82131c5ba2b2a76d37a6",
+    "url": "/AudioAnnotator/static/js/2.5cc72baa.chunk.js"
+  },
+  {
+    "revision": "30ae5137e6e6aa2e66db",
+    "url": "/AudioAnnotator/static/css/main.2792e9b7.chunk.css"
+  },
+  {
+    "revision": "b789b725138663f2701e0ac38df1cb0a",
+    "url": "/AudioAnnotator/index.html"
+  }
+];
